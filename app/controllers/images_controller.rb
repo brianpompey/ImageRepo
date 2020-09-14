e020f5f3f73a56dca37a1a3fb6de8ab7e1005f46
brianpompey <brianthescholar@gmail.com>
2020-09-14 19:21:26 -0400

    lots of work done

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,33 @@
 class ImagesController < ApplicationController
+  #  before_action :require_login
+  #  skip_before_action :require_login, only: [:index, :show]
+
+
     def index
         @images = Image.all
+    end
+
+    def new
+        @image = Image.new
+        @user = current_user
+    end
+
+    def create
+       current_user.images.create!(image_params)
+       redirect_to root_path
+    end
+
+    def show
+        @image = Image.find(params[:id])
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def image_params
+        params.require(:image).permit(:name, :attachment, :description, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'images#index'
-  resources :images, only: [:index, :new, :create, :destroy]
+  resources :images, only: [:index, :new, :create, :show, :destroy]
 end
